c799189c327014bf00882a1095dd3d5fd7b7948d
Andy Shevchenko <ur4ltz@gmail.com>
2021-12-06 20:46:28 +0200

    Update options config.

--- lua/options.lua
+++ lua/options.lua
@@ -2,8 +2,13 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.mouse = 'n'
 vim.opt.number = true
 vim.opt.list   = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+
+vim.opt.clipboard = 'unnamedplus'
 
 vim.o.termguicolors = true
 
