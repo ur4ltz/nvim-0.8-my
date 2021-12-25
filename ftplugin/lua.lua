bc10bd2d65c358fab52226cb28bd9a1a1bd03dbb
Andy Shevchenko <ur4ltz@gmail.com>
2021-12-25 14:28:33 +0200

    Update ftplugin.

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,4 +1,5 @@
-vim.bo.shiftwidth  = 2
-vim.bo.tabstop     = 2
-vim.bo.softtabstop = 2
-vim.bo.textwidth   = 120
+vim.opt.shiftwidth  = 2
+vim.opt.tabstop     = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab   = true
+vim.opt.textwidth   = 120
