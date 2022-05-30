afc5477d212145a6289e7138d70f91c9e8de6434
Andy Shevchenko <ur4ltz@gmail.com>
2022-05-30 13:57:18 +0300

    Update dressing.

--- lua/config/dressing.lua
+++ lua/config/dressing.lua
@@ -7,25 +7,36 @@ require('dressing').setup({
     -- Default prompt string
     default_prompt = "➤ ",
 
+    -- Can be 'left', 'right', or 'center'
+    prompt_align = "left",
+
     -- When true, <Esc> will close the modal
     insert_only = true,
 
     -- These are passed to nvim_open_win
     anchor = "SW",
-    relative = "cursor",
-    row = 0,
-    col = 0,
     border = "rounded",
+    -- 'editor' and 'win' will default to being centered
+    relative = "cursor",
 
     -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
     prefer_width = 40,
-    max_width = nil,
-    min_width = 20,
+    width = nil,
+    -- min_width and max_width can be a list of mixed types.
+    -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+    max_width = { 140, 0.9 },
+    min_width = { 20, 0.2 },
 
     -- Window transparency (0-100)
     winblend = 10,
     -- Change default highlight groups (see :help winhl)
     winhighlight = "",
+
+    override = function(conf)
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      return conf
+    end,
 
     -- see :help dressing_get_config
     get_config = nil,
@@ -38,10 +49,9 @@ require('dressing').setup({
     backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
 
     -- Options for telescope selector
-    telescope = {
-      -- can be 'dropdown', 'cursor', or 'ivy'
-      theme = "dropdown",
-    },
+    -- These are passed into the telescope picker directly. Can be used like:
+    -- telescope = require('telescope.themes').get_ivy({...})
+    telescope = nil,
 
     -- Options for fzf selector
     fzf = {
@@ -75,10 +85,9 @@ require('dressing').setup({
     builtin = {
       -- These are passed to nvim_open_win
       anchor = "NW",
-      relative = "cursor",
-      row = 0,
-      col = 0,
       border = "rounded",
+      -- 'editor' and 'win' will default to being centered
+      relative = "editor",
 
       -- Window transparency (0-100)
       winblend = 10,
@@ -86,12 +95,20 @@ require('dressing').setup({
       winhighlight = "",
 
       -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      -- the min_ and max_ options can be a list of mixed types.
+      -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
       width = nil,
-      max_width = 0.8,
-      min_width = 40,
+      max_width = { 140, 0.8 },
+      min_width = { 40, 0.2 },
       height = nil,
       max_height = 0.9,
-      min_height = 10,
+      min_height = { 10, 0.2 },
+
+      override = function(conf)
+        -- This is the config that will be passed to nvim_open_win.
+        -- Change values here to customize the layout
+        return conf
+      end,
     },
 
     -- Used to override format_item. See :help dressing-format
