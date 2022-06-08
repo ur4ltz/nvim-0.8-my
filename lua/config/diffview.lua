8071ad5f00ac2a19b48fc7477eac8bd24ad98f8e
Andy Shevchenko <ur4ltz@gmail.com>
2022-06-08 16:57:59 +0300

    Update diffview.

--- lua/config/diffview.lua
+++ lua/config/diffview.lua
@@ -3,7 +3,7 @@ local actions = require('diffview.actions')
 
 require('diffview').setup({
   diff_binaries = false,    -- Show diffs for binaries
-  enhanced_diff_hl = false, -- See |diffview-config-enhanced_diff_hl|
+  enhanced_diff_hl = false, -- See ':h diffview-config-enhanced_diff_hl'
   use_icons = true,         -- Requires nvim-web-devicons
   icons = {                 -- Only applies when use_icons is true.
     folder_closed = "",
@@ -19,33 +19,33 @@ require('diffview').setup({
       flatten_dirs = true,              -- Flatten dirs that only contain one single dir
       folder_statuses = "always"        -- One of 'never', 'only_folded' or 'always'.
     },
-    win_config = {                      -- See |diffview-config-win_config|
+    win_config = {                      -- See ':h diffview-config-win_config'
       position = "left",
       width = 35,
     },
   },
   file_history_panel = {
-    log_options = {
-      max_count = 256,      -- Limit the number of commits
-      follow = false,       -- Follow renames (only for single file)
-      all = false,          -- Include all refs under 'refs/' including HEAD
-      merges = false,       -- List only merge commits
-      no_merges = false,    -- List no merge commits
-      reverse = false,      -- List commits in reverse order
+    log_options = {   -- See ':h diffview-config-log_options'
+      single_file = {
+        diff_merges = 'combined',
+      },
+      multi_file = {
+        diff_merges = 'first-parent',
+      },
     },
-    win_config = {          -- See |diffview-config-win_config|
+    win_config = {    -- See ':h diffview-config-win_config'
       position = "bottom",
       height = 16,
     },
   },
   commit_log_panel = {
-    win_config = {},  -- See |diffview-config-win_config|
+    win_config = {},  -- See ':h diffview-config-win_config'
   },
   default_args = {    -- Default args prepended to the arg-list for the listed commands
     DiffviewOpen = {},
     DiffviewFileHistory = {},
   },
-  hooks = {},         -- See |diffview-config-hooks|
+  hooks = {},         -- See ':h diffview-config-hooks'
   keymaps = {
     disable_defaults = false, -- Disable the default keymaps
     view = {
@@ -53,7 +53,7 @@ require('diffview').setup({
       -- tabpage is a Diffview.
       ["<tab>"]      = actions.select_next_entry, -- Open the diff for the next file
       ["<s-tab>"]    = actions.select_prev_entry, -- Open the diff for the previous file
-      ["gf"]         = actions.goto_file,         -- Open the file in a new split in previous tabpage
+      ["gf"]         = actions.goto_file,         -- Open the file in a new split in the previous tabpage
       ["<C-w><C-f>"] = actions.goto_file_split,   -- Open the file in a new split
       ["<C-w>gf"]    = actions.goto_file_tab,     -- Open the file in a new tabpage
       ["<leader>e"]  = actions.focus_files,       -- Bring focus to the files panel
