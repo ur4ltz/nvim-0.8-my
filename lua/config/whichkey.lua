e36bd3470ad3327d65002558a302e2513ad0055f
Andy Shevchenko <ur4ltz@gmail.com>
2022-08-18 22:20:00 +0300

    Update which-key config

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -30,7 +30,6 @@ local mappings = {
     c = {'<cmd>Telescope neoclip<cr>',    'Clipboard'},
     e = {'<cmd>Telescope env<cr>',        'Environment Variables'},
     f = {'<cmd>Telescope find_files<cr>', 'Find files'},
-    g = {'<cmd>Telescope git_files<cr>',  'Git files'},
     h = {'<cmd>Telescope help_tags<cr>',  'Help tags'},
     l = {'<cmd>Telescope live_grep<cr>',  'Live grep'},
     r = {'<cmd>Telescope registers<cr>',  'Registers'},
@@ -52,6 +51,7 @@ local mappings = {
     name = 'Git',
     t = {'<cmd>lua _TIG_TOGGLE()<cr>',      'Tig'},
 
+    g = {'<cmd>Telescope git_files<cr>',    'Git files'},
     o = {'<cmd>Telescope git_status<cr>',   'Open changed files'},
     b = {'<cmd>Telescope git_branches<cr>', 'Checkout branch'},
     c = {'<cmd>Telescope git_commits<cr>',  'Checkout commit'},
