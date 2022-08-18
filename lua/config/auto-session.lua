8de8c45d3f8a4a5b30b01ec2eec88f262c5e5e52
Andy Shevchenko <ur4ltz@gmail.com>
2022-08-18 21:41:02 +0300

    Update auto-session config

--- lua/config/auto-session.lua
+++ lua/config/auto-session.lua
@@ -1,5 +1,14 @@
 
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
 
-require('auto-session').setup()
+require('auto-session').setup {
+  auto_session_enable_last_session = false,
+  auto_session_enabled             = true,
+  auto_session_create_enabled      = false,
+  auto_save_enabled                = nil,
+  auto_restore_enabled             = nil,
+  auto_session_suppress_dirs       = nil,
+  auto_session_allowed_dirs        = nil,
+  auto_session_use_git_branch      = nil,
+}
 
